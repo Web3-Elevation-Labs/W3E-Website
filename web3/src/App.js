712a611416255e687d3078c3b04d91b7a605566a
kadenoseen11 <81404205+kadenoseen11@users.noreply.github.com>
2022-06-21 11:12:57 -0700

    footer

--- web3/src/App.js
+++ web3/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Landing from './Landing/Landing';
 import Services from './Services/Services';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Landing/>
       <Services/>
+      <Footer/>
     </div>
   );
 }
---
+++ web3/src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css'
+
+class Services extends Component {
+
+    render() {
+        return (
+            <div className='footer'>
+            </div>
+        );
+    }
+}
+
+export default Services;
