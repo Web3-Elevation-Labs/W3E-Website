6cfd16fcdbd233d47c6892674f8cd4c670b6e546
Ryans889 <rsewell21@icloud.com>
2022-06-21 13:41:16 -0700

    c

--- web3/src/Footer/Footer.js
+++ web3/src/Footer/Footer.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import './Footer.css'
+import {twitterRedirect} from '../Utils/Redirects'
+import {linkedinRedirect} from '../Utils/Redirects'
+import {thugcitywebsiteRedirect} from '../Utils/Redirects'
+import {w3eofficeRedirect} from '../Utils/Redirects'
 
 class Footer extends Component {
 
     render() {
         return (
             <div className='footer'>
-            </div>
+                <div className='container'>
+                 <div className='column_1'>
+                        <h1> Work </h1>
+                        <ul className='list-unstyled_1'>
+                            <li onClick={() => {thugcitywebsiteRedirect()}}> ThugCity </li>
+                            <li> Careers </li>
+                        </ul>
+                    </div>
+                <div className='column_2'>
+                        <h1> Follow us </h1>
+                        <ul className='list-unstyled_2'>
+                            <li onClick={() => {twitterRedirect()}}> Twitter </li> 
+                            <li> Facebook </li>
+                            <li onClick={() => {linkedinRedirect()}}> Linkedin </li>
+                        </ul>
+                 </div>  
+                    <div className='column_3'>
+                        <h1> Contact Us </h1>
+                        <ul className='list-unstyled_3'>
+                            <li> Admin@w3elabs.com </li>
+                            <li onClick={() => {w3eofficeRedirect()}}> 1988 Triumph St, B.C, Canada </li>
+                        </ul>
+                    </div>
+                </div> 
+                <div className='footer_copyright_text'>
+                        <h2> © 2022 Web3 Elevation labs, Inc. </h2>
+                    </div>
+            </div> 
+ 
+                    
         );
     }
 }
---
+++ web3/src/Utils/Redirects.js
@@ -0,0 +1,21 @@
+
+export const instagramRedirect = () => {
+    window.open("https://instagram.com/thugcityp2e");
+}
+
+export const twitterRedirect = () => {
+    window.open("https://twitter.com/thugcityp2e");
+}
+
+export const linkedinRedirect = () => {
+    window.open("https://www.linkedin.com/company/80217772/admin/");
+}
+
+export const thugcitywebsiteRedirect = () => {
+    window.open("https://www.thugcity.io/");
+}
+
+export const w3eofficeRedirect = () => {
+    window.open("https://www.google.com/maps/place/Web3+Elevation+Labs+Inc/@49.2838266,-123.0664795,17z/data=!3m1!4b1!4m5!3m4!1s0x54867133f9b301a9:0xe3e012a06f8056dc!8m2!3d49.2838266!4d-123.0642908");
+}
+
